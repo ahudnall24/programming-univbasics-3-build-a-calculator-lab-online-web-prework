817ed6a51bb84fbb7fbe2c91ba526463f5eb366e
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-06 00:24:06 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,22 +1,21 @@
 def addition(num1, num2)
-num1 + num2=(sum)
+
   
   
 
 end
 
 def subtraction(num1, num2)
-  num2 - num1=(sum)
-
+ 
 end
 
 def division(num1, num2)
-  num2/num1=(sum)
+  
 
 end
 
 def multiplication(num1, num2)
-  num1 * num2=(sum)
+  
 
 end
 
