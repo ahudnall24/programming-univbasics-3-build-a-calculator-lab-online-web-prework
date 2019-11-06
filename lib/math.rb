bace151f095b72c00f0a01626b70464b8cace76e
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-06 00:17:21 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,20 +1,27 @@
 def addition(num1, num2)
+num1 + num2=(sum)
+  
+  
 
 end
 
 def subtraction(num1, num2)
+  num2 - num1=(sum)
 
 end
 
 def division(num1, num2)
+  num2 \ num1=(sum)
 
 end
 
 def multiplication(num1, num2)
+  num1 * num2=(sum)
 
 end
 
 def modulo(num1, num2)
+  
 
 end
 
