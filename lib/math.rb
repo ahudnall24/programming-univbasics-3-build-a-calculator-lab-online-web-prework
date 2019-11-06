3acad033c23ad19d88274b770250d32e332c0c73
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-06 00:38:25 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,27 +1,27 @@
 def addition(num1, num2)
-  prints num2 + num1
+  print num2 + num1
   
 end
 
 def subtraction(num1, num2)
-  puts num2-num1
+  print num2-num1
  
 end
 
 def division(num1, num2)
-  puts num2/num1
+  print num2/num1
   
 
 end
 
 def multiplication(num1, num2)
-  puts num1 * num2
+  print num1 * num2
   
 
 end
 
 def modulo(num1, num2)
-
+  print (sum)
 
   
 
