f153b3e109c5279a22dc2814fbff8476f57d8ee2
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-06 00:38:38 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,22 +1,31 @@
 def addition(num1, num2)
-
+  print num2 + num1
+  
 end
 
 def subtraction(num1, num2)
-
+  print num2-num1
+ 
 end
 
 def division(num1, num2)
+  print num2/num1
+  
 
 end
 
 def multiplication(num1, num2)
+  print num1 * num2
+  
 
 end
 
 def modulo(num1, num2)
+  print (sum)
 
-end
+  
+
+
 
 def square_root(num)
 
